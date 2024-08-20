63af1c372b319b6a48f7f2d8616256b6db680f1d
jacopolegovini <legovini96@gmail.com>
2024-08-20 15:54:52 +0200

    right arrow added js

--- script.js
+++ script.js
@@ -4,16 +4,36 @@ const arrowLeftElement = document.querySelector('.fa-arrow-left');
 const arrowRightElement = document.querySelector('.fa-arrow-right');
 
 
+
 // Creazione variabili
 const imgs = ['img/01.webp', 'img/02.webp', 'img/03.webp', 'img/04.webp', 'img/05.webp'];
-let htmlElement;
+let htmlElement = '';
+
 
 // Costruzione dinamica della stringa nel DOM
 for (let i = 0; i < imgs.length; i++) {
     const img = imgs[i];
-    htmlElement += `<img src="${img}" alt="img-${i + 1}" class="active">`
+    htmlElement += `<img src="${img}" alt="img-${i + 1}">`
 }
 carouselElement.innerHTML = htmlElement;
 
-console.log(htmlElement)
 
+// Prendiamo gli elementi del DOM creati col ciclo for
+const imgElement = document.querySelectorAll('.carousel-photo img');
+
+
+// Rendiamo visibile la prima img
+let currentIndex = 0;
+imgElement[currentIndex].classList.add('active');
+
+
+// Arrow left & right
+arrowRightElement.addEventListener('click', function(){
+    
+    imgElement[currentIndex].classList.remove('active');
+    
+    currentIndex ++;
+
+    imgElement[currentIndex].classList.add('active');
+
+})
